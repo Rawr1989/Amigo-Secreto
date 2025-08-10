8eca8290a193951797d8bad665db48c53e3e0219
Marco Rivera <ws.rivera@hotmail.com>
2025-08-10 01:28:26 -0500

    actualiza de app.js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 /* El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 Aquí deberás desarrollar la lógica para resolver el problema. */
 
-let amigos = [];
+let listaAmigos = [];
+//Implementa una función para agregar amigos
+function agregarAmigo() {
+    const ingresarAmigo = document.getElementById("amigo");
+    const amigo = ingresarAmigo.value.trim();
 
+    if (amigo !== "") {
+        listaAmigos.push(amigo);
+        ingresarAmigo.value = ""; // Limpiar el campo de entrada
+//        mostrarListaAmigos();
+        console.log(listaAmigos)
 
-
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+}
